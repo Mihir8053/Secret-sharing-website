e82220bd989ab4ed7c6a393ac4c569700b95332d
Mihir Shah <shahmihir2002@gmail.com>
2022-07-04 23:53:46 +0530

    Edit callback URL for secrets page

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ passport.deserializeUser(function(id, done){
 passport.use(new GoogleStrategy({
     clientID:     process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://ancient-scrubland-88072.herokuapp.com/auth/google/secrets",
     // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     passReqToCallback   : true
   },
